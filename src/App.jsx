69a7ac5d51fe995de47c6c7392074cea463ca393
Bạch Thành Vinh <153177368+bachthanhvinh@users.noreply.github.com>
2025-07-20 23:05:26 +0700

    dark_light_mode_system (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,76 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Typography from '@mui/material/Typography'
 import {
-  Experimental_CssVarsProvider as CssVarsProvider,
-  experimental_extendTheme as extendTheme,
-  useColorScheme,
+  // Experimental_CssVarsProvider as CssVarsProvider,
+  // experimental_extendTheme as extendTheme,
+  useColorScheme
 } from '@mui/material/styles'
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+function ModeSmall() {
+  const { mode, setMode } = useColorScheme()
+
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="select-dark-light-mode"
+        id="select=dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }} >
+            <LightModeIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }} >
+            <SettingsBrightnessIcon fontSize='small' />Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value='system'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }} ></Box><Box style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+            <DarkModeOutlinedIcon fontSize='small'/> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 function App() {
 
-  function ModeToggle() {
-    const { mode, setMode } = useColorScheme()
-    return (
-      <Button
-        onClick={() => {
-          setMode(mode === 'light' ? 'dark' : 'light')
-        }}
-      >
-        {mode === 'light' ? 'Turn dark' : 'Turn light'}
-      </Button>
-    )
-  }
+
+  // function ModeToggle() {
+  //   const { mode, setMode } = useColorScheme()
+
+  //   return (
+  //     <Button
+  //       onClick={() => {
+  //         setMode(mode === 'light' ? 'dark' : 'light')
+  //       }}
+  //     >
+  //       {mode === 'light' ? 'Turn dark' : 'Turn light'}
+  //     </Button>
+  //   )
+  // }
+
   return (
     <>
-      <ModeToggle />
+      <ModeSmall />
+      <br />
+      <hr />
+      {/* <ModeToggle /> */}
+      <hr />
       <div>bachthanhvinh</div>
       <Typography variant='body2' color="text.secondary" >study</Typography>
       <Button variant="contained">Hello world</Button>
