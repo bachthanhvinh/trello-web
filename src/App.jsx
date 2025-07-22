4369801ecae8bf4305c612db9f0a3091d36ab5d2
Bạch Thành Vinh <153177368+bachthanhvinh@users.noreply.github.com>
2025-07-22 23:00:43 +0700

    basic layout trello (#7)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import './App.css'
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import Typography from '@mui/material/Typography'
 import {
   // Experimental_CssVarsProvider as CssVarsProvider,
   // experimental_extendTheme as extendTheme,
   useColorScheme
 } from '@mui/material/styles'
-import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import Container from '@mui/material/Container'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-function ModeSmall() {
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+function ModeSelector() {
   const { mode, setMode } = useColorScheme()
 
 
@@ -53,35 +50,35 @@ function ModeSmall() {
 function App() {
 
 
-  // function ModeToggle() {
-  //   const { mode, setMode } = useColorScheme()
-
-  //   return (
-  //     <Button
-  //       onClick={() => {
-  //         setMode(mode === 'light' ? 'dark' : 'light')
-  //       }}
-  //     >
-  //       {mode === 'light' ? 'Turn dark' : 'Turn light'}
-  //     </Button>
-  //   )
-  // }
-
   return (
-    <>
-      <ModeSmall />
-      <br />
-      <hr />
-      {/* <ModeToggle /> */}
-      <hr />
-      <div>bachthanhvinh</div>
-      <Typography variant='body2' color="text.secondary" >study</Typography>
-      <Button variant="contained">Hello world</Button>
-      <Button variant="text">Contained</Button>
-      <Button variant="outlined">Oulined</Button>
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh', backgroundColor: 'primary.main' }}>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        height: (theme) => theme.trello.appBarHeight,
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'end'
+      }}>
+        <ModeSelector />
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        height: (theme) => theme.trello.boardBarHeight,
+        width: '100%'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width: '100%',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board content
+      </Box>
+    </Container>
   )
 }
 export default App
