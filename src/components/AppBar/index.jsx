414b940ae27d861286b76aabfe4822d920654226
Bạch Thành Vinh <153177368+bachthanhvinh@users.noreply.github.com>
2025-07-29 23:25:24 +0700

    app_bar_header_responsive (#12)

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -11,51 +11,120 @@ import Starred from './Menus/Starred'
 import Templates from './Menus/Templates'
 import TextField from '@mui/material/TextField'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
-import { Badge, Tooltip } from '@mui/material'
+import { Badge, Menu, MenuItem, Tooltip } from '@mui/material'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import Profiles from './Menus/Profiles'
 
 function AppBar() {
+
+  const MenusHeader = [Workspaces, Recent, Starred, Templates]
+
   return (
-    <Box sx={{
-      height: (theme) => theme.trello.appBarHeight,
-      width: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: '0px 10px'
-    }}>
-      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <AppsIcon sx={{ color: 'primary.main' }} />
+    <>
+      <Box sx={{
+        height: (theme) => theme.trello.appBarHeight,
+        width: '100%',
+        display: { xs: 'none', md: 'flex' },
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '0px 10px',
+        gap: 2,
+        overflowX: 'auto'
+      }}>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-          <SvgIcon component={TrelloLogo} inheritViewBox fontSize='small' sx={{ color: 'primary.main' }} />
-          <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bolđ', color: 'primary.main' }} >Trello</Typography>
+          <AppsIcon sx={{ color: 'primary.main' }} />
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+            <SvgIcon component={TrelloLogo} inheritViewBox fontSize='small' sx={{ color: 'primary.main' }} />
+            <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bolđ', color: 'primary.main' }} >Trello</Typography>
+          </Box>
+          <Workspaces />
+          <Recent />
+          <Starred />
+          <Templates />
+          <Button variant="outlined">Create</Button>
         </Box>
-        <Workspaces />
-        <Recent />
-        <Starred />
-        <Templates />
-        <Button variant="outlined">Create</Button>
-      </Box>
 
-      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }} >
-        <TextField id="outlined-search" size='small' label="Search" type="search" />
-        <ModeSelector />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, }} >
+          <TextField id="outlined-search" size='small' label="Search" type="search" />
+          <ModeSelector />
 
-        <Tooltip title="Notifications">
-          <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer', color: 'primary.main' }} >
-            <NotificationsNoneIcon />
-          </Badge>
-        </Tooltip>
+          <Tooltip title="Notifications">
+            <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer', color: 'primary.main' }} >
+              <NotificationsNoneIcon />
+            </Badge>
+          </Tooltip>
 
-        <Tooltip title="Help" sx={{ cursor: 'pointer', color: 'primary.main' }}>
-          <HelpOutlineIcon />
-        </Tooltip>
+          <Tooltip title="Help" sx={{ cursor: 'pointer', color: 'primary.main' }}>
+            <HelpOutlineIcon />
+          </Tooltip>
 
-        <Profiles />
+          <Profiles />
+
+        </Box>
 
       </Box>
-    </Box>
+      <Box sx={{
+        height: (theme) => theme.trello.appBarHeight,
+        display: { xs: 'flex', md: 'none' },
+        justifyContent: 'space-between',
+        padding: '0px 10px'
+      }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+          {/* <AppsIcon sx={{ color: 'primary.main' }} /> */}
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{ display: { xs: 'block', md: 'none' } }}
+          >
+            {MenusHeader.map((nav) => (
+              <MenuItem key={nav} onClick={handleCloseNavMenu}>
+                <Typography sx={{ textAlign: 'center' }}>{nav}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+            <SvgIcon component={TrelloLogo} inheritViewBox fontSize='small' sx={{ color: 'primary.main' }} />
+            <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bolđ', color: 'primary.main' }} >Trello</Typography>
+          </Box>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <Workspaces />
+            <Recent />
+            <Starred />
+            <Templates />
+            <Button variant="outlined">Create</Button>
+          </Box>
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, }} >
+          <TextField id="outlined-search" size='small' label="Search" type="search" sx={{ minWidth: '120px' }} />
+          <ModeSelector />
+
+          <Tooltip title="Notifications">
+            <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer', color: 'primary.main' }} >
+              <NotificationsNoneIcon />
+            </Badge>
+          </Tooltip>
+
+          <Tooltip title="Help" sx={{ cursor: 'pointer', color: 'primary.main' }}>
+            <HelpOutlineIcon />
+          </Tooltip>
+
+          <Profiles />
+
+        </Box>
+      </Box>
+    </>
   )
 }
 
--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -15,7 +15,7 @@ function ModeSelector() {
   }
 
   return (
-    <FormControl size="small">
+    <FormControl size="small" sx={{ minWidth: '120px' }}>
       <InputLabel id="select-dark-light-mode">Mode</InputLabel>
       <Select
         labelId="select-dark-light-mode"
