645308b8c93dd1c5603ef7f5e1b21321588e089f
Vinhz <bachthanhvinh12@gmail.com>
2025-08-03 23:20:02 +0700

    board_content

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -1,15 +1,86 @@
+import { Button } from '@mui/material'
 import Box from '@mui/material/Box'
+
+const COLUMN_HEADER_HEIGHT = '50px'
+const COLUMN_FOOTER_HEIGHT = '56px'
 
 function BoardContent() {
   return (
     <Box sx={{
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
       width: '100%',
-      height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-      display: 'flex',
-      alignItems: 'center'
+      height: (theme) => theme.trello.boardContentHeight,
+      // display: 'flex',
+      // gap: 2,
+      // ml: 2
+
     }}>
-        Board content
+      <Box sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? 'blue' : '#ecf0f1'),
+      }} >
+        <Box sx={{
+          height: COLUMN_HEADER_HEIGHT,
+          p: 2,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}>
+          Comlumn Header
+        </Box>
+        <Box sx={{
+        }}>
+          <Box sx={{
+            bgcolor: '#1abc9c',
+            margin: '10px',
+            padding: '15px',
+            borderRadius: '5px'
+          }}
+          >Card</Box>
+
+          <Box sx={{
+            bgcolor: '#1abc9c',
+            margin: '10px',
+            padding: '15px',
+            borderRadius: '5px'
+          }}
+          >Card</Box>
+          <Box sx={{
+            bgcolor: '#1abc9c',
+            margin: '10px',
+            padding: '15px',
+            borderRadius: '5px'
+          }}
+          >Card</Box>
+          <Box sx={{
+            bgcolor: '#1abc9c',
+            margin: '10px',
+            padding: '15px',
+            borderRadius: '5px'
+          }}
+          >Card</Box>
+        </Box>
+        <Button sx={{
+          height: COLUMN_FOOTER_HEIGHT,
+          p: 2,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}>+ Add Card</Button>
+      </Box>
+      <Box sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? 'blue' : 'red'),
+        padding: '0 10px'
+      }} >
+        {/* <Box sx={{
+          height: COLUMN_FOOTER_HEIGHT
+        }} >
+          Footer
+        </Box> */}
+      </Box>
     </Box>
   )
 }
--- src/theme.js
+++ src/theme.js
@@ -3,11 +3,16 @@ import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { BorderRight, Height, WidthFull } from '@mui/icons-material'
 // import { BorderColor } from '@mui/icons-material'
 
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+
 // Create a theme instance.
 const theme = extendTheme({
   trello: {
-    appBarHeight: '58px',
-    boardBarHeight: '60px'
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT
   },
   colorSchemes: {
     // light: {
