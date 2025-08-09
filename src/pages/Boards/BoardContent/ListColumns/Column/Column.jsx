52f339cf7cbbc58c6c48f4433335b3fabe8a83c9
Bạch Thành Vinh <153177368+bachthanhvinh@users.noreply.github.com>
2025-08-09 22:45:08 +0700

    drag_drop_card (#22)

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -21,16 +21,21 @@ import { mapOrder } from '~/utils/sorts'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 
+
 const Column = ({ column }) => {
 
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
     id: column._id,
     data: { ...column }
   })
 
   const dndkitColumnStyles = {
     transform: CSS.Translate.toString(transform),
-    transition
+    transition,
+    // chiếu cao max 100% vì bị lỗi khi kéo từ một column ngắn sang một có column 100%
+    // bị khựng nên phải chỉnh toàn bộ lên 100%
+    height: '100%',
+    opacity: isDragging ? 0.5 : undefined
   }
 
   const [anchorEl, setAnchorEl] = useState(null)
@@ -41,128 +46,132 @@ const Column = ({ column }) => {
   const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
 
   return (
-    <Box
+    <div
       style={dndkitColumnStyles}
       ref={setNodeRef}
       {...attributes}
-      {...listeners}
-      sx={{
-        minWidth: '300px',
-        maxWidth: '300px',
-        ml: 2,
-        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
-        borderRadius: '6px',
-        height: 'fit-content',
-        maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(3)})`,
-        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px'
-      }} >
-      <Box sx={{
-        height: (theme) => (theme.trello.columnHeaderHeight),
-        p: 2,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-      }}>
-        <Typography variant='h6' sx={{
-          fontSize: '1rem',
-          fontWeight: 'bold',
-          cursor: 'pointer'
-        }} > {column?.title} </Typography>
-        {/* Box column Header  */}
-        <Box>
-          <Tooltip title="More Options">
-            <ExpandMoreIcon
-              sx={{
-                color: 'text.primary',
-                cursor: 'pointer'
+    >
+      <Box
+      // để đây vì muốn khu vực nào được kéo thì để ở đó vì listenners
+        {...listeners}
+        sx={{
+          minWidth: '300px',
+          maxWidth: '300px',
+          ml: 2,
+          bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
+          borderRadius: '6px',
+          height: 'fit-content',
+          maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(3)})`,
+          boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px'
+        }} >
+        <Box sx={{
+          height: (theme) => (theme.trello.columnHeaderHeight),
+          p: 2,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}>
+          <Typography variant='h6' sx={{
+            fontSize: '1rem',
+            fontWeight: 'bold',
+            cursor: 'pointer'
+          }} > {column?.title} </Typography>
+          {/* Box column Header  */}
+          <Box>
+            <Tooltip title="More Options">
+              <ExpandMoreIcon
+                sx={{
+                  color: 'text.primary',
+                  cursor: 'pointer'
+                }}
+                id="basic-column-dropdown"
+                aria-controls={open ? 'basic-menu-column-dropdown' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+              />
+            </Tooltip>
+            <Menu
+              id="basic-menu-column-dropdown"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              slotProps={{
+                list: {
+                  'aria-labelledby': 'basic-column-dropdown'
+                }
               }}
-              id="basic-column-dropdown"
-              aria-controls={open ? 'basic-menu-column-dropdown' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              onClick={handleClick}
-            />
-          </Tooltip>
-          <Menu
-            id="basic-menu-column-dropdown"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            slotProps={{
-              list: {
-                'aria-labelledby': 'basic-column-dropdown'
-              }
-            }}
-          >
-            <MenuItem>
-              <ListItemIcon>
-                <AddCardIcon fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Add new card</ListItemText>
-            </MenuItem>
-            <MenuItem>
-              <ListItemIcon>
-                <ContentCut fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Cut</ListItemText>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                ⌘X
-              </Typography>
-            </MenuItem>
-            <MenuItem>
-              <ListItemIcon>
-                <ContentCopy fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Copy</ListItemText>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                ⌘C
-              </Typography>
-            </MenuItem>
-            <MenuItem>
-              <ListItemIcon>
-                <ContentPaste fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Paste</ListItemText>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                ⌘V
-              </Typography>
-            </MenuItem>
-            <Divider />
-            <MenuItem>
-              <ListItemIcon>
-                <DeleteForeverIcon fontSize="small" />
-              </ListItemIcon>
-              <ListItemText> Remove this column</ListItemText>
-            </MenuItem>
-            <MenuItem>
-              <ListItemIcon>
-                <Cloud fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Web Clipboard</ListItemText>
-            </MenuItem>
-          </Menu>
+            >
+              <MenuItem>
+                <ListItemIcon>
+                  <AddCardIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Add new card</ListItemText>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <ContentCut fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Cut</ListItemText>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  ⌘X
+                </Typography>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <ContentCopy fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Copy</ListItemText>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  ⌘C
+                </Typography>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <ContentPaste fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Paste</ListItemText>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  ⌘V
+                </Typography>
+              </MenuItem>
+              <Divider />
+              <MenuItem>
+                <ListItemIcon>
+                  <DeleteForeverIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText> Remove this column</ListItemText>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <Cloud fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Web Clipboard</ListItemText>
+              </MenuItem>
+            </Menu>
+          </Box>
+
         </Box>
 
+        {/* box List cards */}
+
+        <ListCards cards={orderedCards} />
+
+        {/* box footer */}
+        <Box sx={{
+          height: (theme) => (theme.trello.columnFooterHeight),
+          p: 2,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}>
+          <Button startIcon={<AddCardIcon />}>Add new Card</Button>
+          <Tooltip title='Drag to move' >
+            <DragHandleIcon sx={{ cursor: 'pointer' }} />
+          </Tooltip>
+        </Box>
       </Box>
-
-      {/* box List cards */}
-
-      <ListCards cards={orderedCards} />
-
-      {/* box footer */}
-      <Box sx={{
-        height: (theme) => (theme.trello.columnFooterHeight),
-        p: 2,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-      }}>
-        <Button startIcon={<AddCardIcon />}>Add new Card</Button>
-        <Tooltip title='Drag to move' >
-          <DragHandleIcon sx={{ cursor: 'pointer' }} />
-        </Tooltip>
-      </Box>
-    </Box>
+    </div>
   )
 }
 
--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,31 +1,35 @@
 import Box from '@mui/material/Box'
 import Card from './Card/Card'
+import { horizontalListSortingStrategy, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
+
 
 const ListCards = ({ cards }) => {
 
   return (
-    <Box sx={{
-      p: '0 5px',
-      m: '0 5px',
-      display: 'flex',
-      flexDirection: 'column',
-      gap: 1,
-      overflowX: 'hidden',
-      overflowY: 'auto',
-      maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} -
-      ${theme.spacing(3)} -
-      ${theme.trello.columnHeaderHeight} -
-      ${theme.trello.columnFooterHeight}
-      )`,
-      '&::-webkit-scrollbar-thumb': { backgroundColor: '#bec0cdff' },
-      '&::-webkit-scrollbar-thumb:hover': { backgroundColor: '#aaafc4ff' }
-    }}
-    >
-      {/* Card */}
-      {cards?.map(card => <Card key={card._id} card={card} />)}
+    <SortableContext items={cards?.map(c => c._id)} strategy={verticalListSortingStrategy}>
 
+      <Box sx={{
+        p: '0 5px',
+        m: '0 5px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 1,
+        overflowX: 'hidden',
+        overflowY: 'auto',
+        maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} -
+        ${theme.spacing(3)} -
+        ${theme.trello.columnHeaderHeight} -
+        ${theme.trello.columnFooterHeight}
+        )`,
+        '&::-webkit-scrollbar-thumb': { backgroundColor: '#bec0cdff' },
+        '&::-webkit-scrollbar-thumb:hover': { backgroundColor: '#aaafc4ff' }
+      }}
+      >
+        {/* Card */}
+        {cards?.map(card => <Card key={card._id} card={card} />)}
+      </Box>
+    </SortableContext>
 
-    </Box>
   )
 }
 
