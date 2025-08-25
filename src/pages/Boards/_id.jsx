da977ec217612527b4c92afb69fab5f31f7c6c71
Bạch Thành Vinh <153177368+bachthanhvinh@users.noreply.github.com>
2025-08-25 14:43:08 +0700

    mongodb_aggregate (#31)

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,20 +2,22 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
 import { fetchBoardDetailsApi } from '~/apis'
 function Board() {
   const [board, setBoard] = useState(null)
 
   useEffect( () => {
-    const boardId = '68aabb72a22e7e58cb7b78d1'
+    const boardId = '68aad23695a530a3c2e42035'
 
     fetchBoardDetailsApi(boardId).then(board => {
       setBoard(board)
     })
+
   }, [])
-  console.log(board)
+
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
