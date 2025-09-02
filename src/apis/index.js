b61819099a505844182a6c2ccebb3b5df59bf34c
bachthanhvinh <bachthanhvinh12@gmail.com>
2025-09-02 22:41:20 +0700

    move_card_update

--- src/apis/index.js
+++ src/apis/index.js
@@ -16,6 +16,10 @@ export const createColumnApi = async (newColumn) => {
   const response = await axios.post(`${API_ROOT}/v1/columns`, newColumn)
   return response.data
 }
+export const updateColumnDetailsApi = async (columnId, newCard) => {
+  const response = await axios.put(`${API_ROOT}/v1/columns/${columnId}`, newCard)
+  return response.data
+}
 
 export const createCardApi = async (newCard) => {
   const response = await axios.post(`${API_ROOT}/v1/cards`, newCard)
