e614a40d86ff943a3ba63172422939bf3afdda2d
bachthanhvinh <bachthanhvinh12@gmail.com>
2025-09-11 22:02:52 +0700

    Fix BUILD_MODE dev and production.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,15 @@
+let apiRoot = ''
+
+console.log('import.meta.env', import.meta.env)
+console.log('import.meta.env', process.env)
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-4crl.onrender.com'
+}
+export const API_ROOT = apiRoot
+
+console.log('apiRoot', apiRoot)
 // export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-4crl.onrender.com'
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  // cho phép thằng vite sử dụng được process.env, mặc định thì khồng mà sẽ phải dùng import.meta.env
+  // https://github.com/vitejs/vite/issues/1973
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
     svgr()
