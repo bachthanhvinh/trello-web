037d4acfd096a07c00a1d8fd5b04921724556f15
bachthanhvinh <bachthanhvinh12@gmail.com>
2025-09-12 23:46:13 +0700

    commet code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,15 +1,16 @@
 let apiRoot = ''
 
-console.log('import.meta.env', import.meta.env)
-console.log('import.meta.env', process.env)
-
+// console.log('import.meta.env', import.meta.env)
+// console.log('import.meta.env', process.env)
+// Nếu là local dev thì sẽ chạy vào phần local
 if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
+// Nếu là production thì sẽ chạy vào đây
 if (process.env.BUILD_MODE === 'production') {
   apiRoot = 'https://trello-api-4crl.onrender.com'
 }
 export const API_ROOT = apiRoot
 
-console.log('apiRoot', apiRoot)
+// console.log('apiRoot', apiRoot)
 // export const API_ROOT = 'http://localhost:8017'
