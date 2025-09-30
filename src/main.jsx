82ebe948eb23f949dfb579888cb4a86f32f9ebe8
Bạch Thành Vinh <153177368+bachthanhvinh@users.noreply.github.com>
2025-09-30 23:55:06 +0700

    handle HttpOnly Cookie, AuthMiddleware, Redux Persist, Protected Routes (#47)

--- src/main.jsx
+++ src/main.jsx
@@ -16,23 +16,30 @@ import { store } from '~/redux/store'
 // Cấu hình react-router-dom với  BrowerRouter
 import { BrowserRouter } from 'react-router-dom'
 
+// cấu hình Redux-Persist
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+const persistor = persistStore(store)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <BrowserRouter basename='/'>
     <Provider store={store}>
-      <CssVarsProvider theme={theme}>
-        <ConfirmProvider defaultOptions={{
-          allowClose: false,
-          dialogProps: { maxWidth: 'xs' },
-          cancellationButtonProps: { color: 'inherit' },
-          confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
-          buttonOrder: ['confirm', 'cancel']
-        }}>
-          <CssBaseline />
-          <App />
-          <ToastContainer theme="colored" position="bottom-left" />
-        </ConfirmProvider>
-      </CssVarsProvider>
+      <PersistGate persistor={ persistor }>
+        <CssVarsProvider theme={theme}>
+          <ConfirmProvider defaultOptions={{
+            allowClose: false,
+            dialogProps: { maxWidth: 'xs' },
+            cancellationButtonProps: { color: 'inherit' },
+            confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
+            buttonOrder: ['confirm', 'cancel']
+          }}>
+            <CssBaseline />
+            <App />
+            <ToastContainer theme="colored" position="bottom-left" />
+          </ConfirmProvider>
+        </CssVarsProvider>
+      </PersistGate>
     </Provider>
   </BrowserRouter>
   // {/* </React.StrictMode> */}
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,7 +1,9 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation, Navigate } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import LoginForm from './LoginForm'
 import RegisterForm from './RegisterForm'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from '~/redux/user/userSlice'
 
 function Auth() {
   const location = useLocation()
@@ -9,6 +11,10 @@ function Auth() {
   const isLogin = location.pathname === '/login'
   const isRegister = location.pathname === '/register'
 
+  const currentUser = useSelector(selectCurrentUser)
+  if (currentUser) {
+    return <Navigate to='/' replace={true} />
+  }
   return (
     <Box sx={{
       display: 'flex',
