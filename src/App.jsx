b78d8f0b5e8eb67ed49e8c6997458b8ec9b2758e
bachthanhvinh <bachthanhvinh12@gmail.com>
2025-09-30 23:54:22 +0700

    handle HttpOnly Cookie, AuthMiddleware, Redux Persist, Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Board from '~/pages/Boards/_id'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 import NotFound from './pages/404/NotFound'
 import Auth from './pages/Auth/Auth'
 import AccountVerification from './pages/Auth/AccountVerification'
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from './redux/user/userSlice'
+
+/**
+ * Giải pháp Clean Code trong việc xác định các route nào cần đăng nhập tài khoản xong thì mới cho truy cập
+ * Sử dụng <Outlet /> của react-router-dom để hiển thị các child Route (xem cách sử dụng trong App() bên dưới)
+ */
+const ProtectedRoute = ({ user }) => {
+  if (!user) return <Navigate to='/login' replace={true} />
+  return <Outlet />
+}
 function App() {
+  const currentUser = useSelector(selectCurrentUser)
+
   return (
     <Routes>
       {/* Redirect Route */}
@@ -11,8 +24,14 @@ function App() {
         // ở đây cần replace giá trị true để nó thay thế route /, có thể hiểu là route / sẽ không còn nằm trong history của Brower
         <Navigate to='/boards/68c19236c00be1c92fe3ab14' replace={true} />
       } />
-      {/* Board Detail */}
-      <Route path='/boards/:boardId' element={<Board />}/>
+      {/* Protected Routes Hiểu đơn giản trong dự án của là những route chỉ cho truy cập sau khi
+      đã login */}
+      <Route element={ <ProtectedRoute user={currentUser} /> }>
+        {/* <Outlet/> là của react-router-dom sẽ chạy vào các child route trong này */}
+
+        {/* Board Detail */}
+        <Route path='/boards/:boardId' element={<Board />}/>
+      </Route>
 
       {/* Authentication */}
       <Route path='/login' element={<Auth/>}/>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { activeBoardReducer } from './activeBoard/activeBoardSlice'
 import { userReducer } from './user/userSlice'
+import { combineReducers } from 'redux' // lưu ý chúng ta cần có sẵn redux trong node_modules bởi vì khi cài @reduxjs/toolkit là đã có luôn
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // default là localstorage
+
+// Cấu hình persist
+const rootPersistConfig = {
+  key: 'root', // key của cái persist do chúng ta chỉ định, cứ để mặc định là root
+  storage, // Biến storage ở trên - lưu vào localstorage
+  whitelist: ['user'] //  định nghĩa các slice dữ liệu được phép duy trì qua mỗi lần f5 trình duyệt
+  // blacklist: ['user] // định nghĩa các slice Không được phép duy trì qua mỗi lần f5 trình duyệt
+}
+
+// Combine các reducers trong dự án của chúng ta ở đây
+const reducers = combineReducers({
+  activeBoard: activeBoardReducer,
+  user: userReducer
+})
+
+// Thực hiện persist Reducer
+const persistedReducers = persistReducer(rootPersistConfig, reducers)
+
+
 export const store = configureStore({
-  reducer: {
-    activeBoard: activeBoardReducer,
-    user: userReducer
-  }
+  reducer: persistedReducers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
 })
